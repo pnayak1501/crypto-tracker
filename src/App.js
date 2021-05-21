7f1e890710894373ffdddf7ccc94cc6e9740a649
Prahlad <prahlad.is18@bmsce.ac.in>
2021-05-21 23:41:22 +0530

    try2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Coin from './Coin';
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 
-
-
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState('');
