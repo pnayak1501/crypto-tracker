242bd94015ae7380f43c89a1063d40ff3d25d64b
Prahlad <prahlad.is18@bmsce.ac.in>
2021-05-21 22:03:55 +0530

    5th commit

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
       </div>
       {filteredCoins.map(coin => {
         return(
-          <Coin key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} volume={coin.market_cap} price={coin.current_price} /> 
+          <Coin key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} marketcap={coin.market_cap} price={coin.current_price} priceChange={coin.price_change_percentage_24h} volume={coin.total_volume} /> 
         )
       })}
     </div>
