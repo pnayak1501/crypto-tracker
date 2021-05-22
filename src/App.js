045348c43a8343b69ac1c50f09ba567f3f75dfee
pnayak1501 <pnayak1501@gmail.com>
2021-05-22 10:13:35 +0530

    Hurrayyyyyyyyy

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import './App.css';
 import axios from 'axios'
 import Coin from './Coin';
 
-
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false
-
-
-
-
 
 function App() {
   const [coins, setCoins] = useState([]);
